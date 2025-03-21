0de0001a7807f94211a837fc8f0ad9ea19f0e9f4
Buddhsen Tripathi <buddhsent19@gmail.com>
2025-03-21 16:36:11 +0530

    removed unwanted log

--- 2.0/lib/supabaseClient.ts
+++ 2.0/lib/supabaseClient.ts
@@ -3,7 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('Supabase ANON KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
