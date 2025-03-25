049e663ab0892b18d30ec9af6da77513896e8a67
Buddhsen Tripathi <buddhsent19@gmail.com>
2025-03-25 20:13:34 +0530

    readtime util pack addition

--- 2.0/lib/utils.ts
+++ 2.0/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getReadingTime(content: string): number {
+  const wordsPerMinute = 200;
+  const words = content.trim().split(/\s+/).length;
+  return Math.ceil(words / wordsPerMinute);
+}
