c7f3b83db05885ae841dab37d137097795346d71
Buddhsen Tripathi <buddhsent19@gmail.com>
2025-03-26 03:16:41 +0530

    New blog posted

---
+++ 2.0/next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+import withMDX from "@next/mdx";
+
+const nextConfig: NextConfig = withMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})({
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+});
+
+export default nextConfig;
